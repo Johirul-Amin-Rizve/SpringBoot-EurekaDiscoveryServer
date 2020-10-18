40ca3849704c5f1c54204b721008697560797576
Johirul-Amin-Rizve <johirulaminrizve@gmail.com>
2020-10-18 14:31:27 +0600

    create rest template bean

--- movie-catalog-service/src/main/java/com/learning/moviecatalogservice/resource/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/com/learning/moviecatalogservice/resource/MovieCatalogResource.java
@@ -3,6 +3,7 @@ package com.learning.moviecatalogservice.resource;
 import com.learning.moviecatalogservice.models.CatalogItem;
 import com.learning.moviecatalogservice.models.Movie;
 import com.learning.moviecatalogservice.models.Rating;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,16 +11,19 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/catalog")
 public class MovieCatalogResource {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
     @GetMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable int userId){
-        RestTemplate restTemplate = new RestTemplate();
+
 
         List<Rating> ratings = Arrays.asList(
                 new Rating(1 , 1),
