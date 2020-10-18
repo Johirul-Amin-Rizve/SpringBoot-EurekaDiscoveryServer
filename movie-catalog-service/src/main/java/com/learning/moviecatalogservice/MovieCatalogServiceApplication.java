7166440bf5696e4f4c0feb4ed8cc77cb65dc10ee
Johirul-Amin-Rizve <johirulaminrizve@gmail.com>
2020-10-18 16:01:34 +0600

    eureka server configure

--- movie-catalog-service/src/main/java/com/learning/moviecatalogservice/MovieCatalogServiceApplication.java
+++ movie-catalog-service/src/main/java/com/learning/moviecatalogservice/MovieCatalogServiceApplication.java
@@ -2,11 +2,13 @@ package com.learning.moviecatalogservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.reactive.function.client.WebClient;
+//import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MovieCatalogServiceApplication {
 
 	//rest template request
